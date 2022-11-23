8956456b7db3ea09b599578f16beb147af038848
feven27 <110137693+feven27@users.noreply.github.com>
2022-11-22 19:57:04 -0500

    R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,2 +1,24 @@
 library(dplyr)
+library(magrittr)
 
+Mechacar_mpg_df <- read.csv(file='../resources/MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+
+head(Mechacar_mpg_df)
+
+lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = Mechacar_mpg_df)
+
+summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = Mechacar_mpg_df))
+
+suspension_coil_df <- read.csv(file = '../resources/Suspension_coil.csv', check.names = F, stringsAsFactors = F)
+
+head(suspension_coil_df)
+
+total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+t.test(suspension_coil_df$PSI, mu=1500)
+
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
